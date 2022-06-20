665f9162ff335df6d7fdc1d7970f69209ce8a10b
Sayyed-Abrar-Akhtar <58516708+Sayyed-Abrar-Akhtar@users.noreply.github.com>
2022-06-20 12:53:56 +0545

    updated the app with shop page containing collection preview and collection item components

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
+
+import './collection-preview.styles.scss';
+
+const CollectionPreview = ({ title, items }) => (
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {/* Performance concern => Anonymous function calls inside any components do get called again and rerender brand new when this component render or rerender  */}
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
+    </div>
+  </div>
+);
+export default CollectionPreview;
