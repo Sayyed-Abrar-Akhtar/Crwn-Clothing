b8b14b5308d23c103e6251a781df542b30b31dab
Sayyed-Abrar-Akhtar <58516708+Sayyed-Abrar-Akhtar@users.noreply.github.com>
2022-06-25 23:23:54 +0545

    Added redux config, reducers, actions and implemented in the app

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
+import React from 'react';
+
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
-import Header from './components/header/header.component';
-import SignInAndSignUpPage from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import React from 'react';
 import { onSnapshot } from 'firebase/firestore';
 
+import { connect } from 'react-redux';
+
+import './App.css';
+
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import { setCurrentUser } from './redux/user/user.actions';
+
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null,
-    };
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     //auth.onAuthStateChanged return a function that is assigned to "unsubscribeFromAuth" properties of the class which is initially null
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
-        console.log(userAuth);
         const userRef = await createUserProfileDocument(userAuth);
 
         onSnapshot(userRef, (snapshot) => {
-          this.setState({
-            currentUser: { id: snapshot.id, ...snapshot.data() },
-          });
+          setCurrentUser({ id: snapshot.id, ...snapshot.data() });
         });
       } else {
-        this.setState({ currentUser: userAuth });
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -47,7 +45,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -58,4 +56,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
